dd1a6902fefb1462225703425ff124094ef30c82
Celson Gutkowski <idkdavidc@gmail.com>
2018-12-26 16:46:41 -0500

    basic calculator

---
+++ calculator.js
@@ -0,0 +1,14 @@
+function insert(num) {
+    document.calculator.display.value = document.calculator.display.value+num;
+}
+
+function equal() {
+  var exp = document.calculator.display.value;
+  if(exp) {
+    document.calculator.display.value = eval(exp);
+  }
+}
+
+function clean() {
+  document.calculator.display.value = "";
+}
