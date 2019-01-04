c6b31eca75ff1403c52655cbb6d1cd64bc78231d
Celson Gutkowski <idkdavidc@gmail.com>
2019-01-04 13:21:24 -0500

    minor changes

--- calculator.js
+++ calculator.js
@@ -7,8 +7,22 @@ function equal() {
   if(exp) {
     document.calculator.display.value = eval(exp);
   }
+  if(document.calculator.display.value === "Infinity") {
+    document.calculator.display.value = "Error";
+  }
+  if(document.calculator.display.value === "undefined") {
+    document.calculator.display.value = "Error";
+  }
 }
 
 function clean() {
   document.calculator.display.value = "";
+}
+
+function negation() {
+  document.calculator.display.value = document.calculator.display.value * -1;
+}
+
+function percentage() {
+  document.calculator.display.value = document.calculator.display.value / 100;
 }
