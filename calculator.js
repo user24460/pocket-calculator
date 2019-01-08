b080726118d5d09272fd0b96313f43a8a6bb7bb0
Celson Gutkowski <idkdavidc@gmail.com>
2019-01-08 13:19:19 -0500

    clearing display when clicking on buttons

--- calculator.js
+++ calculator.js
@@ -1,24 +1,32 @@
+var evalStr = '';
+var lastNum = true; var lastSym = false; var lastEql = false;
 function insert(num) {
-    document.calculator.display.value = document.calculator.display.value + num;
+  if (lastEql) {
+    clean(); lastEql = false;
+  }
+  if (lastNum) {
+    document.calculator.display.value = (document.calculator.display.value + num);
+    lastSym = true;
+  } else {
+    document.calculator.display.value = (num);
+    lastSym = true; lastNum = true;
+  }
+  evalStr = evalStr + num;
 }
-
+function parseSym(sym) {
+  if (lastSym) {
+    document.calculator.display.value = (sym);
+    lastSym = false; lastNum = false; lastEql = false; evalStr = evalStr + sym;
+  }
+}
 function equal() {
-  var exp = document.calculator.display.value;
-  if(exp) {
-    document.calculator.display.value = eval(exp);
-  }
-  if(document.calculator.display.value === "Infinity") {
-    document.calculator.display.value = "Error";
-  }
-  if(document.calculator.display.value === "undefined") {
-    document.calculator.display.value = "Error";
-  }
+  document.calculator.display.value = eval(evalStr);
+  evalStr = eval(evalStr).toLocaleString("en"); lastNum = false; lastEql = true;
 }
-
 function clean() {
-  document.calculator.display.value = "";
+  document.calculator.display.value = ' ';
+  evalStr = ''; lastNum = true; lastSym = false;
 }
-
 function negation() {
   document.calculator.display.value = document.calculator.display.value * -1;
 }
@@ -26,3 +34,30 @@ function negation() {
 function percentage() {
   document.calculator.display.value = document.calculator.display.value / 100;
 }
+// function insert(num) {
+//     document.calculator.display.value = document.calculator.display.value + num;
+// }
+//
+// function equal() {
+//   var exp = document.calculator.display.value;
+//   if(exp) {
+//     document.calculator.display.value = eval(exp);
+//   }
+//   if(document.calculator.display.value === "Infinity") {
+//     document.calculator.display.value = "Error";
+//   }
+//   if(document.calculator.display.value === "undefined") {
+//     document.calculator.display.value = "Error";
+//   }
+// }
+// function clean() {
+//   document.calculator.display.value = "";
+// }
+//
+// function negation() {
+//   document.calculator.display.value = document.calculator.display.value * -1;
+// }
+//
+// function percentage() {
+//   document.calculator.display.value = document.calculator.display.value / 100;
+// }
